667be10965e2648f8b3836bd2cfdb0c3439a317c
sddtc <changhbaga@gmail.com>
2024-03-11 19:46:35 +0800

    Component - bind type to a class with default constructor

--- src/main/java/container/Context.java
+++ src/main/java/container/Context.java
@@ -1,17 +1,30 @@
 package container;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Context {
 
-  private final Map<Class<?>, Object> components = new HashMap<Class<?>, Object>();
+  private final Map<Class<?>, Object> components = new HashMap<>();
+  private final Map<Class<?>, Class<?>> componentsWithImplementations = new HashMap<>();
 
   public <C> void bind(Class<C> type, C instance) {
     components.put(type, instance);
   }
 
   public <C> C get(Class<C> type) {
-    return (C) components.get(type);
+    if (components.containsKey(type)) {
+      return (C) components.get(type);
+    }
+    try {
+      return (C) componentsWithImplementations.get(type).getConstructor().newInstance();
+    } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public <C, I extends C> void bind(Class<C> type, Class<I> implementation) {
+    componentsWithImplementations.put(type, implementation);
   }
 }
--- src/test/java/container/ContainerTest.java
+++ src/test/java/container/ContainerTest.java
@@ -3,7 +3,9 @@ package container;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ContainerTest {
 
@@ -27,6 +29,22 @@ public class ContainerTest {
   }
 
   @Nested
+  public class ConstructorInjection {
+
+    @Test
+    void should_bind_type_to_a_class_with_default_constructor() {
+      Context context = new Context();
+
+      context.bind(Component.class, ComponentWithDefaultConstructor.class);
+      Component instance = context.get(Component.class);
+
+      assertNotNull(instance);
+      assertTrue(instance instanceof ComponentWithDefaultConstructor);
+    }
+
+  }
+
+  @Nested
   public class DependencySelection {
 
   }
@@ -35,4 +53,12 @@ public class ContainerTest {
   public class LifecycleManagement {
 
   }
+}
+
+class ComponentWithDefaultConstructor implements ContainerTest.Component {
+
+  public ComponentWithDefaultConstructor() {
+
+  }
+
 }
